c48ae4ca3cd7990ead7bfc9a74ec6890dea59ba2
tinychoy <49453614+tinychoy@users.noreply.github.com>
2020-11-10 10:48:16 +0800

    Add files via upload

--- wowfont.bat
+++ wowfont.bat
@@ -1,13 +1,18 @@
 @echo off
+title = 魔兽国服字体修改脚本
 CLS
 
 color 0a
-ECHO 
-ECHO 此脚本的行为：
-ECHO 1、将拖放到脚本的文件复制改名到脚本所在目录里对应字体文件名；
-ECHO 2、将脚本目录里对应的文件复制备份为 ...BAK.TTF；
+ECHO. 
+ECHO. 此脚本的行为：
+ECHO. 1、将拖放到脚本的文件复制改名到脚本所在目录里对应字体文件名；
+ECHO. 2、将脚本目录里对应的文件复制备份为 "...BAK.TTF"；
 ECHO.
 ECHO.魔兽国服字体修改脚本 By 凌晨凉风-格瑞姆巴托
+ECHO.
+ECHO.
+ECHO.是(1)否(0)备份文件为...BAK.TTF输入(1,0)回车:
+set /p bak=
 set fp=%1
 if not defined fp (GOTO fp)else (GOTO MENU)
 
@@ -20,7 +25,10 @@ goto MENU
 PAUSE
 
 :MENU
-ECHO. 
+ECHO
+if "%bak%"=="1" ECHO   *你选择备份相应文件为"...BAK.TTF"
+if "%bak%"=="0" ECHO   *你选择不备份！
+ECHO.  *你选择的字体为：%fp%
 ECHO.
 ECHO.  =-=-=-=-=-= 将选择字体改为以下字体 =-=-=-=-=-=
 ECHO.
@@ -42,7 +50,6 @@ ECHO.  8  *** 更换新字体 ***
 ECHO.
 ECHO.  0  退   出
 ECHO.
-ECHO. 
 ECHO.
 ECHO.输入选择对应序号回车确定：
 set /p  ID=
@@ -59,40 +66,54 @@ PAUSE
 
 
 :cmd1
-copy "%~dp0ARKai_C.TTF" "%~dp0ARKai_Cbak.TTF"
+if "%bak%"=="1" copy "%~dp0ARKai_C.TTF" "%~dp0ARKai_Cbak.TTF"
 copy %fp% "%~dp0ARKai_C.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 
 :cmd2
-copy "%~dp0ARKai_T.TTF" "%~dp0ARKai_Tbak.TTF"
+if "%bak%"=="1" copy "%~dp0ARKai_T.TTF" "%~dp0ARKai_Tbak.TTF"
 copy %fp% "%~dp0ARKai_T.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 
 :cmd3
-copy "%~dp0ARHei.TTF" "%~dp0ARHeibak.TTF"
+if "%bak%"=="1" copy "%~dp0ARHei.TTF" "%~dp0ARHeibak.TTF"
 copy %fp% "%~dp0ARHei.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 :cmd4
-copy "%~dp0FRIZQT__.TTF" "%~dp0FRIZQT__bak.TTF"
+if "%bak%"=="1" copy "%~dp0FRIZQT__.TTF" "%~dp0FRIZQT__bak.TTF"
 copy %fp% "%~dp0FRIZQT__.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 :cmd5
-copy "%~dp0ARIALN.TTF" "%~dp0ARIALNbak.TTF"
+if "%bak%"=="1" copy "%~dp0ARIALN.TTF" "%~dp0ARIALNbak.TTF"
 copy %fp% "%~dp0ARIALN.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 :cmd6
-copy "%~dp0FZBWJW.TTF" "%~dp0FZBWJWbak.TTF"
+if "%bak%"=="1" copy "%~dp0FZBWJW.TTF" "%~dp0FZBWJWbak.TTF"
 copy %fp% "%~dp0FZBWJW.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 :cmd7
-copy "%~dp0FZXHJW.TTF" "%~dp0FZXHJWbak.TTF"
+if "%bak%"=="1" copy "%~dp0FZXHJW.TTF" "%~dp0FZXHJWbak.TTF"
 copy %fp% "%~dp0FZXHJW.TTF"
+cls
+IF ERRORLEVEL 0 ECHO 字体更改成功！
 GOTO MENU
 
 :cmd8
